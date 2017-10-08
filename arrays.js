d13047712639d86715f1795bc17bcf4489346223
Joseph Mawson <jmawson95@gmail.com>
2017-10-08 16:01:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,5 +26,5 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  console.log(accessElementInArray[2]);
+  return accessElementInArray[2]
 }
